5cedf8327f8ec0d2310f08498ba040e86456712b
ArdeshirV <e-job@protonmail.com>
2025-09-30 20:18:56 +0330

    Update

--- array-vs-slice.go
+++ array-vs-slice.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+)
+
+const (
+	N = 100
 )
 
 func main() {
@@ -10,10 +15,34 @@ func main() {
 	SliceAlgorithms()
 }
 
-func ArrayAlgorithms() {
-
+func ArrayAlgorithms() int {
+	var arr [N]int
+	sum := 0
+	for i := range N {
+		arr[i] = i * i
+	}
+	for i := N - 1; i >= 0; i-- {
+		sum += arr[i]
+	}
+	for range N {
+		j := rand.Intn(N)
+		sum ^= arr[j]
+	}
+	return sum
 }
 
-func SliceAlgorithms() {
-
+func SliceAlgorithms() int {
+	slice := make([]int, N)
+	sum := 0
+	for i := range N {
+		slice[i] = i * i
+	}
+	for i := N - 1; i >= 0; i-- {
+		sum += slice[i]
+	}
+	for range N {
+		j := rand.Intn(N)
+		sum ^= slice[j]
+	}
+	return sum
 }
